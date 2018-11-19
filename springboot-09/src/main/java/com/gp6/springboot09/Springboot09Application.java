dd490c4d05a48a9749894c4bb7ea50340ea78c46
gp6 <pfgao@ips.com>
2018-11-19 18:05:42 +0800

    9 :环境配置文件切换-Profile

---
+++ springboot-09/src/main/java/com/gp6/springboot09/Springboot09Application.java
@@ -0,0 +1,13 @@
+package com.gp6.springboot09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class Springboot09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09Application.class, args);
+    }
+}
