7c4c6336d31967748513859e597c33623e068edb
gp6 <pfgao@ips.com>
2018-11-29 17:18:59 +0800

    17:扩展与全部自定义SpringMVC

---
+++ springboot-17/src/main/java/com/gp6/springboot17/config/MyConfig.java
@@ -0,0 +1,22 @@
+package com.gp6.springboot17.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/*
+    使用WebMvcConfigurationSupport可扩展SpringMVC的功能
+
+    @EnableWebMvc: SpringBoot对SpringMVC自动配置全部失效(不推荐)
+*/
+//@EnableWebMvc
+@Configuration
+public class MyConfig extends WebMvcConfigurationSupport {
+
+    // 添加视图映射
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //浏览器发送 "/gp6" 请求,来到 success页面(由于页面不存在,500错误)
+        registry.addViewController("/gp6").setViewName("success");
+    }
+}
