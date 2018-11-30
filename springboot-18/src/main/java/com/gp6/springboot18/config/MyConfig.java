3111beff66c6b5344dbd71317391af870c1b5040
gp6 <pfgao@ips.com>
2018-11-30 15:14:28 +0800

    20:Web开发实战--登录&拦截器

--- springboot-18/src/main/java/com/gp6/springboot18/config/MyConfig.java
+++ springboot-18/src/main/java/com/gp6/springboot18/config/MyConfig.java
@@ -3,6 +3,7 @@ package com.gp6.springboot18.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,20 @@ public class MyConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
         registry.addViewController("/main.html").setViewName("dashboard");
+    }
+
+    //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /*
+            SpringBoot已配置静态资源映射,编写拦截器时,无需考虑静态资源
+        */
+        registry.addInterceptor(new MyHandlerInterceptor()).
+                // 拦截的请求(/** : 所有)
+                addPathPatterns("/**")
+
+                // 放行的请求
+                .excludePathPatterns("/login.html","/","/user/login");
     }
 
     /*
